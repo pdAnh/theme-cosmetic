15d2ce7d822378beafe12bf3146489042d466e5e
pdAnh <pdanh012096@gmail.com>
2018-04-13 13:57:30 +0700

    add responsive for web

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -10,7 +10,6 @@ $(document).ready(function () {
     autoplayHoverPause: true,
     lazyload: true,
     smartSpeed: 900,
-
   })
   $('.list').owlCarousel({
     items: 5,
@@ -19,10 +18,41 @@ $(document).ready(function () {
     smartSpeed: 900,
     navSpeed: 200,
     slideBy: 5,
-    navText: ["<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"]
+    navText: ["<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"],
+    responsive: {
+      0: {
+        items: 2,
+        nav: true
+      },
+      600: {
+        items: 4,
+        nav: false
+      },
+      1000: {
+        items: 5,
+        nav: true,
+        loop: false
+      }
+    }
   })
 
-
+  $('.ok').owlCarousel({
+    items: 3,
+    dots: true,
+    smartSpeed: 900,
+    navSpeed: 200,
+    slideBy: 1,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true
+      },
+      600: {
+        items: 3,
+        nav: false
+      }
+    }
+  })
 });
 
 
--- server.js
+++ server.js
@@ -15,6 +15,9 @@ server.get("/product", (req, res) => {
 server.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html")
 })
+server.get("/category", (req, res) => {
+  res.sendFile(__dirname + "/public/category.html")
+})
 
 server.listen(server.get('port'), (req, res) => {
   console.log("Server is running on port %s...", server.get('port'));
